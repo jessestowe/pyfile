1043cc96dde3c9e7bea58ddae576365412d3fb74
Jesse Stowe <stowejt@gmail.com>
2015-12-08 23:18:04 -0500

    make read function update file if needed

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import pyfile
 def main():	
 	newFile = pyfile.liveFile("testIO", mode="r+")
 	input("press enter to continue")
-	print(newFile.read())
+	output = newFile.read()
+	print(output)
 
 if __name__ == "__main__":
 	main()
--- pyfile.py
+++ pyfile.py
@@ -7,23 +7,29 @@ class liveFile(io.FileIO):
 	def getTimestamp(self):
 		return os.path.getmtime(self.path)
 
-	def getContents(self):
+	def _getContents(self):
 		self.seek(0)
-		return super().read()
+		return super().read().decode()
 
-	def __init__(self, name, mode='r', closefd=True, opener=None):
+	def __init__(self, name, mode="rw", closefd=True, opener=None):
 		super().__init__(name, mode, closefd, opener)
 		self.path = name
+		self.opener = opener
 		self.timestamp = self.getTimestamp()
-		self.contents = self.getContents()
+		self.contents = self._getContents()
 	
 	def isCurrent(self):
 		return (self.getTimestamp() == self.timestamp)
 
 	def read(self):
 		if not self.isCurrent():
-			self.contents = self.getContents()
+			self.close()
+			self.__init__(self.name, self.mode, self.closefd, self.opener)
 		return self.contents
 
-#	def write(self):
+	def write(self, value):
+		if not self.isCurrent():
+			self.close()
+			self.__init__(self.name, self.mode, self.closefd, self.opener)
+		self.write(value)
 		
